59c5bcab5b03ee7144f84c19f21543a3483a46e6
Muhammad Faisal Amir <faisalamircs@gmail.com>
2022-02-11 12:35:05 +0700

    Initial commit

---
+++ app/src/main/java/com/frogobox/githubaction/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.frogobox.githubaction
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
